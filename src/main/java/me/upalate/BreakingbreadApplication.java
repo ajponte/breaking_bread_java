2495cb9923a5b63e31307e640d9caede3d92e24f
Alan Ponte <alan.ponte@locationlabs.com>
2016-01-07 15:22:30 -0800

    First scaffolding attempt

---
+++ src/main/java/me/upalate/BreakingbreadApplication.java
@@ -0,0 +1,12 @@
+package me.upalate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakingbreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakingbreadApplication.class, args);
+	}
+}
